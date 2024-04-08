2b9dc99dae31723940bf993e3af6af60b305cbee
emmanuel-yegon <kptooman@gmail.com>
2024-04-08 17:13:44 +0300

    simple todo app using react-native

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
